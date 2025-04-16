c05160654b4e6d4cae5a3779452d47a9b231c23b
redasx1201 <ashtonxu1201@gmail.com>
2025-04-16 02:04:02 -0600

    fixes

--- PythonPart1.py
+++ PythonPart1.py
@@ -8,16 +8,12 @@ cell_types = ['b_cell', 'cd8_t_cell', 'cd4_t_cell', 'nk_cell', 'monocyte']
 cell_count_file['total_population'] = cell_count_file[cell_types].sum(axis=1)
 
 # Change DataFrame to give each cell type their own row
-new_df = cell_count_file.melt(
-    id_vars=['sample', 'total_population'],
-    value_vars=cell_types,
-    var_name='population',
-    value_name='count'
-)
+new_df = cell_count_file.melt(id_vars=['sample', 'total_population'], value_vars=cell_types,
+                              var_name='cell_type', value_name='cell_population')
 
-new_df['percentage'] = (new_df['count'] / new_df['total_population']) * 100
+new_df['percentage'] = (new_df['cell_population'] / new_df['total_population']) * 100
 
 # Reorder columns so they are like the given instructions
-finalCSV = new_df[['sample', 'total_population', 'population', 'count', 'percentage']]
+finalCSV = new_df[['sample', 'total_population', 'cell_type', 'cell_population', 'percentage']]
 
 finalCSV.to_csv('cell_count_percentages.csv', index=False)
